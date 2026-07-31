479302fb5d6c156a04442ff49829e3b32b075ea4
cualquierverdurarockinfantil-ship-it <cualquierverdurarockinfantil@gmail.com>
2026-07-31 17:51:13 -0300

    Agrega Vercel Analytics al sitio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import PageNotFound from './lib/PageNotFound';
 import ScrollToTop from './components/ScrollToTop';
 import Home from '@/pages/Home';
@@ -21,6 +22,7 @@ function App() {
         </Routes>
       </Router>
       <Toaster />
+      <Analytics />
     </QueryClientProvider>
   )
 }
